1c758007fab8a4c6590a6aab63c9bdef8da4a097
rafasilva9537 <rafasilva9537@gmail.com>
2025-05-15 01:02:41 -0300

    Add properties to medicine interface

--- interfaces/Medicine.ts
+++ interfaces/Medicine.ts
@@ -2,5 +2,7 @@ export default interface Medicine {
     id: number,
     medicineName: string,
     schedule: Date[],
-    isTaken: boolean
+    isTaken: boolean,
+    dosage: number,
+    dosageUnit: string
 }
