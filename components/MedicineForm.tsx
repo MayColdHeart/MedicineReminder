78e63af2ef4ba804ff3b82845a91e6e24885acb3
MayColdHeart <devmayy@gmail.com>
2025-05-19 01:13:31 -0300

    organized code (operational app)

--- components/MedicineForm.tsx
+++ components/MedicineForm.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { medicines } from '@/fake_data/medicines';
 import Medicine from '@/interfaces/Medicine';
+import ColonIcon from '@expo/vector-icons/Entypo';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
 
 type MedicineFormProps = {
     setShowMedicineForm: (showState: React.SetStateAction<boolean>) => void,
@@ -21,18 +23,38 @@ const MedicineForm = ({ setShowMedicineForm, currentMedicineId, updatingMedicine
             id: (medicineLastId + 1),
             medicineName: "",
             schedule: [],
-            dosage: 0,
+            dosage: null,
             dosageUnit: "",
         }
     );
 
     const currentMedicineIndex = medicines.findIndex(m => m.id === currentMedicineId);
     const currentMedicine = medicines[currentMedicineIndex];
-    const [updatedMedicine, setUpdatedMedicine] = useState({... currentMedicine});
+    const [updatedMedicine, setUpdatedMedicine] = useState<Medicine>({ ...currentMedicine });
+    const [newHours, setNewHours] = useState(0);
+    const [newMinutes, setNewMinutes] = useState(0);
+
+    const handleNumericInputChange = (value: string | null, key: string) => {
+        if (updatingMedicine) {
+            if (!value) setUpdatedMedicine({ ...updatedMedicine, [key]: null });
+            else setUpdatedMedicine({ ...updatedMedicine, [key]: parseInt(value, 10) });
+        } else {
+            if (!value) setNewMedicine({ ...updatedMedicine, dosage: null });
+            else setNewMedicine({ ...newMedicine, [key]: parseInt(value, 10) });
+        }
+    }
+
+    const handleTextInputChange = (value: string | null, key: string) => {
+        if (updatingMedicine) {
+            setUpdatedMedicine({ ...updatedMedicine, [key]: value });
+        } else {
+            setNewMedicine({ ...newMedicine, [key]: value })
+        }
+    }
 
     return (
         <View style={styles.popupFundo}>
-            <View style={styles.popup}>
+            <ScrollView style={styles.popup}>
                 <Text style={styles.popupTitulo}>
                     {updatingMedicine ? "Editar Remédio" : "Adicionar Remédio"}
                 </Text>
@@ -41,21 +63,94 @@ const MedicineForm = ({ setShowMedicineForm, currentMedicineId, updatingMedicine
                     style={styles.input}
                     placeholder={updatingMedicine !== false ? "Editar medicamento.." : "Digite o nome (Ex: Dipirona)"}
                     value={updatingMedicine ? updatedMedicine.medicineName : newMedicine.medicineName}
-                    onChangeText={(medicineName) => {
-                        if (updatingMedicine) {
-                            setUpdatedMedicine({ ...updatedMedicine, medicineName: medicineName});
-                        } else {
-                            setNewMedicine({ ...newMedicine, medicineName: medicineName })
-                        }
-                    }}
+                    onChangeText={(medicineName) => handleTextInputChange(medicineName, "medicineName")}
                 />
 
-                {/* <TextInput
+                <TextInput
                     style={styles.input}
-                    placeholder={null !== null ? "Editar medicamento.." : "Digite o nome (Ex: Dipirona)"}
-                    value={newMedicine.medicineName}
-                    onChangeText={(medicineName) => {setNewMedicine({...newMedicine, medicineName: medicineName})}}
-                /> */}
+                    placeholder={updatingMedicine !== false ? "Editar dosagem..." : "Digite a dosagem (Ex: 15)"}
+                    defaultValue=''
+                    keyboardType='numeric'
+                    value={
+                        updatingMedicine ? updatedMedicine.dosage?.toString() : newMedicine.dosage?.toString()
+                    }
+                    onChangeText={(dosage) => handleNumericInputChange(dosage, "dosage")}
+                />
+
+                <View style={styles.timeWithAddContainer}>
+                    <View style={styles.timeContainer}>
+                        <TextInput
+                            style={styles.inputTime}
+                            placeholder='00'
+                            keyboardType='numeric'
+                            maxLength={2}
+                            onChangeText={(hour) => setNewHours(parseInt(hour, 10))}
+                        />
+                        <Text style={styles.twoDots}>:</Text>
+                        <TextInput
+                            style={styles.inputTime}
+                            placeholder='00'
+                            keyboardType='numeric'
+                            maxLength={2}
+                            onChangeText={(minute) => setNewMinutes(parseInt(minute, 10))}
+                        />
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            const newDate = new Date(
+                                new Date().getFullYear(),
+                                new Date().getMonth(),
+                                new Date().getDate(),
+                                newHours,
+                                newMinutes,
+                            );
+
+                            if (updatingMedicine) {
+                                updatedMedicine.schedule.push({
+                                    hour: newDate,
+                                    isTaken: false
+                                });
+                                setUpdatedMedicine({ ...updatedMedicine });
+                            } else {
+                                newMedicine.schedule.push({
+                                    hour: newDate,
+                                    isTaken: false
+                                });
+                                setNewMedicine({ ...newMedicine, });
+                            }
+                        }}
+                    >
+                        <Ionicons
+                            name="add-circle-outline"
+                            size={80}
+                            color="#4CAF50"
+                        />
+                    </TouchableOpacity>
+                </View>
+
+                {/* Show edited medicine: list of hours */}
+                {
+                    (updatedMedicine.schedule !== undefined && updatingMedicine === true) ? updatedMedicine.schedule.map(((item, index) =>
+                        <Text key={index} style={styles.savedTime}>
+                            {item.hour.getHours().toString().padStart(2, "0")}
+                            :
+                            {item.hour.getMinutes().toString().padStart(2, "0")}
+                            {" hrs"}
+                        </Text>
+                    )) : <></>
+                }
+
+                {/* Show new medicine: list of hours */}
+                {
+                    (newMedicine.schedule !== undefined && updatingMedicine === false) ? newMedicine.schedule.map(((item, index) =>
+                        <Text key={index} style={styles.savedTime}>
+                            {item.hour.getHours().toString().padStart(2, "0")}
+                            :
+                            {item.hour.getMinutes().toString().padStart(2, "0")}
+                            {" hrs"}
+                        </Text>
+                    )) : <></>
+                }
 
                 <View style={styles.botoesPopup}>
                     <TouchableOpacity
@@ -63,6 +158,8 @@ const MedicineForm = ({ setShowMedicineForm, currentMedicineId, updatingMedicine
                         onPress={() => {
                             setShowMedicineForm(false);
                             setUpdatingMedicine(false);
+                            setNewHours(0);
+                            setNewMinutes(0);
                         }}
                     >
                         <Text style={styles.textButton}>Cancelar</Text>
@@ -74,11 +171,25 @@ const MedicineForm = ({ setShowMedicineForm, currentMedicineId, updatingMedicine
                             setShowMedicineForm(false);
                             setUpdatingMedicine(false);
 
-                            if(updatingMedicine) {
+                            if (updatingMedicine) {
                                 currentMedicine.medicineName = updatedMedicine.medicineName;
-                                currentMedicine.dosageUnit = updatedMedicine.dosageUnit;
                                 currentMedicine.dosage = updatedMedicine.dosage;
-                                //currentMedicine.schedule
+                                currentMedicine.dosageUnit = updatedMedicine.dosageUnit;
+
+                                if(newHours !== 0 && newMinutes !== 0){
+                                    const newDate = new Date(
+                                        new Date().getFullYear(),
+                                        new Date().getMonth(),
+                                        new Date().getDate(),
+                                        newHours,
+                                        newMinutes,
+                                    );
+
+                                    currentMedicine.schedule.push({
+                                        hour: newDate,
+                                        isTaken: false
+                                    });
+                                }
                             }
                             else {
                                 medicines.push(newMedicine);
@@ -88,7 +199,7 @@ const MedicineForm = ({ setShowMedicineForm, currentMedicineId, updatingMedicine
                         <Text style={styles.textButton}>Salvar</Text>
                     </TouchableOpacity>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     );
 }
@@ -110,6 +221,8 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         padding: 20,
         elevation: 10,
+        marginTop: 50,
+        marginBottom: 50
     },
     popupTitulo: {
         fontSize: 20,
@@ -126,15 +239,55 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         fontSize: 16,
     },
+    timeWithAddContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 15,
+    },
+    timeContainer: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 12,
+        borderRadius: 8,
+        fontSize: 16,
+        flexDirection: "row",
+        gap: 15,
+        width: "75%",
+        height: 100,
+        alignSelf: "center"
+    },
+    inputTime: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 12,
+        borderRadius: 8,
+        fontSize: 40,
+        flex: 1,
+    },
+    twoDots: {
+        fontSize: 50,
+        fontWeight: "bold",
+    },
     textButton: {
         color: 'white',
         fontWeight: 'bold',
+    },
+    savedTime: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        padding: 12,
+        marginBottom: 15,
+        borderRadius: 8,
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
     },
     formButton: {
         padding: 12,
         borderRadius: 8,
         width: '48%',
         alignItems: 'center',
+        marginBottom: 50
     },
     cancelButton: {
         backgroundColor: '#e0e0e0',
@@ -142,7 +295,6 @@ const styles = StyleSheet.create({
     saveButton: {
         backgroundColor: '#4caf50',
     },
-
     botoesPopup: {
         flexDirection: 'row',
         justifyContent: 'space-between',
