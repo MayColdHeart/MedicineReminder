e187fa383d8f0a1b79d218ea26676b4af72087a4
rafasilva9537 <rafasilva9537@gmail.com>
2025-06-02 13:26:37 -0300

    Create api solution and project

---
+++ api/MedicineReminder/Program.cs
@@ -0,0 +1,33 @@
+using MedicineReminder.Endpoints;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "Medicine Reminder API";
+        options.Theme = ScalarTheme.Saturn;
+        options.DarkMode = true;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapRootEndpoints();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
